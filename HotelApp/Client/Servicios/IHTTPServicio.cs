8fad9a40ec753a8f038f2122d4a0a0659b643bc5
lucascostamagna19@outlook.com <lucascostamagna19@outlook.com>
2023-09-15 18:19:10 -0300

    Agregar archivos de proyecto.

---
+++ HotelApp/Client/Servicios/IHTTPServicio.cs
@@ -0,0 +1,8 @@
+﻿namespace AppMaquina.Client.Servicios
+{
+    public interface IHTTPServicio
+    {
+        Task<HTTPRespuesta<T>> Get<T>(string url);
+        Task<HTTPRespuesta<object>> Post<T>(string url, T enviar);
+    }
+}
---
+++ Reservas/Data/Entity/Habitacion.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reservas.BData.Data.Entity
+{
+    public class Habitacion
+	{
+		public int Id { get; set; }
+		public string Nhab { get; set; }
+
+		[Required]
+		public int Camas { get; set; }
+
+		[Required(ErrorMessage = "El estado es Obligatorio")]
+		public string Estado { get; set; } = "";
+
+		[Required]
+		public decimal Precio { get; set; }
+		[Required]
+		public decimal Garantia { get; set; }
+		[NotMapped]
+        //public int reservadidhab { get; set; }
+		//[JsonIgnore]
+        public Reserva Reserva { get; set; }
+    }
+}
